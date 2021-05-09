2054a033a9149dfccd62b2920fa55dc7300bc43b
blu3fishez <65532873+blu3fishez@users.noreply.github.com>
2021-05-09 15:09:00 +0900

    Update solution.cpp
    Rollbacked into formal one. & Bug Fixed which is the wrong 2nd array deletion

--- algospot/BOARDCOVER/solution.cpp
+++ algospot/BOARDCOVER/solution.cpp
@@ -9,27 +9,16 @@ const int dir[4][3][2] = {
     {{0,0},{1,0},{1,-1}}
 };
 
-bool set(int y, int x, int** board, int Ax, int Ay, int type, int delta){
-    // I couldn't think about this function.
-    // because of stack overflow.
-    // so that this function is not for used recursion.
-    // but also it does block push/pop.
-    bool canPlace = true; // canPlace variable comes here.
-    for(int i = 0; i<3; ++i){
-        const int ny = y + dir[type][i][0];
-        const int nx = x + dir[type][i][1];
-        if(ny < 0 || ny >= Ay || nx < 0 || nx >= Ax)
-            canPlace = false;
-        else if((board[ny][nx] += delta) > 1)
-            canPlace = false;
-    }
-    return canPlace;
-}
-
-int cover_board(int Ay, int Ax, int** board){
+int cover_board(int y, int x, int** board){
+	bool finish = true;
+	for(int i = 0; i<y; ++i)
+	    for(int j = 0; j<x; ++j)
+	        if(board[i][j] == 0) finish = false;
+	int ret = 0;
     int curx = -1, cury = -1;
-	for(int i = 0; i<Ay; ++i){
-	    for(int j = 0; j<Ax; ++j)
+	if(finish) return 1;
+	for(int i = 0; i<y; ++i){
+	    for(int j = 0; j<x; ++j)
             if(board[i][j] == 0){
                 curx = j;
                 cury = i;
@@ -37,17 +26,29 @@ int cover_board(int Ay, int Ax, int** board){
             }
         if(cury != -1) break;
     }
-    if(cury == -1) return 1;
-    int ret = 0;
-
     // if found most left & top block, start placing.
-    for(int type = 0; type<4; ++type){
-        // i tried to solve this problem through one-function.
-        // but this made the overhead too big, so i divided into 2 functions.
-        // this would be nice tip for exhaustive search.
-        if(set(cury, curx, board, Ay, Ax, type, 1))
-            ret += cover_board(Ay, Ax, board);
-        set(cury, curx, board, Ay, Ax, type, -1);
+    for(int i = 0; i<4; ++i){
+        bool canPlace = true;
+        int nextX, nextY;
+        for(int j = 0; j<3; ++j){
+            nextY = cury + dir[i][j][0];
+            nextX = curx + dir[i][j][1];
+            if(0 > nextY || nextY >= y || 0 > nextX || nextX >= x)
+                canPlace = false;
+            else if(board[nextY][nextX] == 1) canPlace = false;
+        }
+        for(int j = 0; j < 3 && canPlace; ++j){
+            nextY = cury + dir[i][j][0];
+            nextX = curx + dir[i][j][1];
+            board[nextY][nextX] = 1;
+        }
+        if(canPlace) ret += cover_board(y, x, board);
+        // assertion
+        for(int j = 0; j < 3 && canPlace; ++j){
+            nextY = cury + dir[i][j][0];
+            nextX = curx + dir[i][j][1];
+            board[nextY][nextX] = 0;
+        }
     }
 	return ret;
 }
@@ -56,29 +57,6 @@ int main(){
 	int t; cin>>t;
 	while(t--){
         int Ax, Ay;
-<<<<<<< HEAD
-	    string lineInput;
-	    cin >> Ay >> Ax;
-	    int** board = new int*[Ay];
-	    for(int i = 0; i<Ay; ++i)
-	        board[i] = new int[Ax];
-        bool allBlocked = true;
-	    for(int i = 0; i<Ay; ++i){
-            cin>>lineInput;
-            for(int j = 0; j<Ax; ++j){
-                if(lineInput[j] == '#')
-                    board[i][j] = 1;
-                else if(lineInput[j] == '.'){
-                    board[i][j] = 0;
-                    allBlocked = false;
-                }
-	        }
-        }
-        cout<<cover_board(Ay, Ax, board)<<'\n';
-	    for(int i = 0; i<Ay; ++i)
-	        delete[] board[i];
-	    delete[] board;
-=======
 		string lineInput;
 		cin >> Ay >> Ax;
 		int** board = new int*[Ay];
@@ -97,6 +75,5 @@ int main(){
 		for(int i = 0; i<Ay; ++i)
 			delete[] board[i];
 		delete[] board;
->>>>>>> 39875a5589adaaf2f7396b13c157a1175a540803
 	}
 }
